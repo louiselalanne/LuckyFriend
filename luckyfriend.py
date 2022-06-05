d2e937c9572986e8947917f309f0b6de5d223cff
Louise Lalanne <100588945+louiselalanne@users.noreply.github.com>
2022-06-05 16:26:20 -0300

    Update luckyfriend.py

--- luckyfriend.py
+++ luckyfriend.py
@@ -1,4 +1,3 @@
-from logging import PlaceHolder
 from tkinter import *
 import random
 
@@ -7,33 +6,35 @@ root.title("Luck friend Wheel")
 root.geometry("400x400")
 root.configure(background="purple")
 
-entername = Entry(root)
+entername = Entry(root, pla)
 nomes = []
-resultado = Label(root, bg = "purple")
-random_number_label = Label(root, bg = "purple")
+resultado = Label(root, bg = "purple", fg="white", font="Arial 10 bold")
+random_number_label = Label(root, bg = "purple", fg="white", font="Arial 20 bold")
+amigo_escolhido = Label(root, bg = "purple", fg="white", font="Arial 10 bold")
 
 def addfriend():
     nome_amigo = entername.get()
     nomes.append(nome_amigo)
-    resultado["text"] = "Sua lista de amigos: " + str(nomes)
+    resultado["text"] = "Lista: " + str(nomes)
 
 def amigo_aleatorio():
     length = len(nomes)
     indice = random.randint(0, length-1)
     random_number_label["text"] = str(indice)
     pessoa = nomes[indice]
-    resultado["text"] = str(pessoa)
+    amigo_escolhido["text"] = str(pessoa)
 
 btn = Button(root)
 btn = Button(root, text="Adicionar", command=addfriend)
 btn.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-btn = Button(root)
-btn = Button(root, text="Quem será o escolhido?", command=amigo_aleatorio)
-btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+btn2 = Button(root)
+btn2 = Button(root, text="Quem será o escolhido?", command=amigo_aleatorio)
+btn2.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-resultado.place(relx = 0.5, rely=0.7, anchor = CENTER)
+resultado.place(relx = 0.5, rely=0.4, anchor = CENTER)
 random_number_label.place(relx = 0.5, rely=0.6, anchor = CENTER)
 entername.place(relx=0.5, rely=0.2, anchor=CENTER)
+amigo_escolhido.place(relx = 0.5, rely=0.7, anchor = CENTER)
 
 root.mainloop()
